c20d7f4e70de166f12b10265ae00d4e3c73a3089
heathercoraje <heathercoraje@gmail.com>
2018-05-14 23:57:37 +0900

    deploy on heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const router = require('./router');
 
 const server = http.createServer(router);
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`Magic happens on port ${port}`);
